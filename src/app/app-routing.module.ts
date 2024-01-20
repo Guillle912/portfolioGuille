22ff255ca48576241d5c87c7b572029fae5237de
Guillermo Vidal <Guillle912@gmail.com>
2024-01-20 11:46:55 +0100

    Retoques en el portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
+import { AboutComponent } from './modules/about/about.component';
+import { HomeComponent } from './modules/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './modules/home/home.component';
-import { AboutComponent } from './modules/about/about.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '/' },
 
 ];
 
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -12,14 +12,14 @@ export class ProjectService {
       {
         title: 'SGAL Construciones',
         image: 'assets/images/sgalimagen.png',
-        description: 'App de gestion para constructora, contiene un CRUD completo donde puedes dar de alta trabajadores, obras y tareas, asi como borrarlas y modificarlas .',
+        description: 'App de gestión para constructora, contiene un CRUD completo dónde puedes dar de alta trabajadores, obras y tareas, asi como borrarlas y modificarlas .',
         tecnologies: 'Angular, NodeJs, Express, TypeScript, MySQL',
         link: 'https://github.com/Guillle912/back_constructora'
       },
       {
         title: 'FofyConsolas',
         image: 'assets/images/fofyconsolas.jpg',
-        description: 'Blog inspirado en una pagina de videojuegos donde se pueden crear, modificar y borrar posts. Realizado con Angular. ',
+        description: 'Blog inspirado en una página de videojuegos dónde se pueden crear, modificar y borrar posts. Realizado con Angular. ',
         tecnologies: 'Angular, TypeScript, Bootstrap',
         link: 'https://github.com/Guillle912/practica_fangular_blog_guille'
       },
@@ -54,14 +54,14 @@ export class ProjectService {
       {
         title: 'Paises App',
         image: 'assets/images/PaisesApp.jpg',
-        description: 'App hecha con Angular, donde consumo una API con varios EndPoints y pongo en practica una barra de busqueda dinámica.',
+        description: 'App hecha con Angular, dónde consumo una API con varios EndPoints y pongo en práctica una barra de busqueda dinámica.',
         tecnologies: 'Angular, Bootstrap, TypeScript',
         link: 'https://github.com/Guillle912/CountrySPA'
       },
       {
         title: 'Ricky & Morty',
         image: 'assets/images/rickymorty.jpg',
-        description: 'Pagina para practicar el consumo de una API, hacer su paginado y detalle según su ID',
+        description: 'Página para practicar el consumo de una API, hacer su paginado y detalle según su ID',
         tecnologies: 'Angular, TypeScript, Bootstrap',
         link: 'https://github.com/Guillle912/Rick-MortyApp'
       },
